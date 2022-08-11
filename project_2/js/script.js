7ec3fc66699ee5642e3ad61a6c0844c9916dc176
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-08-12 00:03:51 +0500

    project-2/Формирования списка фильма динамически

--- project_2/js/script.js
+++ project_2/js/script.js
@@ -26,7 +26,8 @@ const movieDB = {
 
 const adv = document.querySelectorAll('.promo__adv img'),
       poster = document.querySelector('.promo__bg'),
-      genre = poster.querySelector('.promo__genre');
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
 
 adv.forEach(item => {
     item.remove();
@@ -35,3 +36,16 @@ adv.forEach(item => {
 genre.textContent = "драма";
 
 poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${++i} ${film}
+        <div class="delete"></div>
+    </li>`;
+});
+
+
