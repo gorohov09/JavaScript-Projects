b1c0592303a0a4802fc3f1e83151e89bc85b7e36
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-10-03 16:21:08 +0300

    Использование классов и rest-операторов в работе

--- project_3/js/script.js
+++ project_3/js/script.js
@@ -168,5 +168,80 @@ window.addEventListener('DOMContentLoaded', () => {
     //Открытие модального окна, когда долистали до конца
     window.addEventListener('scroll', showModalByScroll); //Событие выполняется один раз
 
+
+    //Карточки меню(используем классы)
+
+    class MenuCard {
+
+        constructor(imageSrc, imageAlt, title, descr, price, parentSelector, ...classes){
+            this.imageSrc = imageSrc;
+            this.imageAlt = imageAlt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.classes = classes;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27; //Статический курс валют
+            this.changeToUAH(); //Выполнится самым последним
+        }
+
+        //Дополнительный функционал для перевода из долларов в гривны
+        changeToUAH(){
+            this.price = this.price * this.transfer;
+        }
+
+        //Рендеринг верстки
+        render() {
+            const element = document.createElement('div');
+
+            if (this.classes.length == 0)
+                this.classes.push('menu__item');
+
+            //Назначаем классы нашему элементу
+            this.classes.forEach(className => element.classList.add(className));
+
+            element.innerHTML = `
+                <img src=${this.imageSrc} alt=${this.imageAlt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            `;
+
+            //Добавляем вновь созданный элемент внутрь родителя
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg", 
+        "vegy", 
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        229,
+        '.menu .container').render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg", 
+        "elite", 
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        500,
+        '.menu .container',
+        'menu__item').render();
+
+    new MenuCard(
+        "img/tabs/vegy.jpg", 
+        "vegy", 
+        'Меню "Хочу и буду"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        229,
+        '.menu .container',
+        'menu__item').render();
+    
+
 });
 
