512372547aa2634a10a546789d046f29ba9a16d6
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-10-11 16:57:51 +0300

    Реализация простого слайдера

--- project_3/js/script.js
+++ project_3/js/script.js
@@ -332,20 +332,57 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 4000);
     }
 
-    //Тестовый код
-    // fetch('http://localhost:3000/menu')
-    //     .then(data => data.json())
-    //     .then(res => console.log(res));
+    //Slider
 
-    // fetch('https://jsonplaceholder.typicode.com/posts', {
-    //     method: "POST",
-    //     body: JSON.stringify({name: 'Alex'}),
-    //     headers: {
-    //         'Content-type': 'application/json'
-    //     }
-    // })
-    // .then(response => response.json())
-    // .then(json => console.log(json));
+    const slides = document.querySelectorAll('.offer__slide'),
+          prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next'),
+          total = document.querySelector('#total'),
+          current = document.querySelector('#current');
+
+    let slideIndex = 1;
+
+    showSlides(1);
+
+    if (slides.length < 10){
+        total.textContent = `0${slides.length}`;
+    }
+    else{
+        total.textContent = `${slides.length}`;
+    }
+
+    function showSlides(n) {
+        if (n > slides.length){ //Если ушли в правую границу
+            slideIndex = 1;
+        }
+        
+        if (n < 1){
+            slideIndex = slides.length;
+        }
+
+        slides.forEach(item => item.style.display = 'none');
+        slides[slideIndex - 1].style.display = 'block';
+
+        if (slides.length < 10){
+            current.textContent = `0${slideIndex}`;
+        }
+        else{
+            current.textContent = `${slideIndex}`;
+        }
+        
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', () => {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', () => {
+        plusSlides(1);
+    });
 
 });
 
